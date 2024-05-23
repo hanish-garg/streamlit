9f0baa114e9377d235fcb11641322a0ab2d32583
hanish-garg <52251895+hanish-garg@users.noreply.github.com>
2024-05-23 13:11:04 -0400

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,12 @@ st.title('Uber pickups in NYCCC test')
 
 DATE_COLUMN = 'date/time'
 print("Hello World")
-print(DATA_URL)
+
 print(DATE_COLUMN)
 
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
+print(DATA_URL)
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()

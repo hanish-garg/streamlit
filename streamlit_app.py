f77d55937ef7f91a4aa6aad7a9912c9a0ec56afd
hanish-garg <52251895+hanish-garg@users.noreply.github.com>
2024-05-23 13:27:00 -0400

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,7 @@ def load_data(nrows):
     st.text("reading data")
     st.text(data)
     lowercase = lambda x: str(x).lower()
-    st.text("lowercase")
-    st.text(lowercase)
+     
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     st.text("reading data again and again")
@@ -42,5 +41,4 @@ st.bar_chart(hist_values)
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
